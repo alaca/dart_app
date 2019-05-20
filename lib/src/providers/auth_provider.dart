51a4c0b3f5696c23ea2b4a35d535f24c17b1f6fc
alaca <ante.laca@gmail.com>
2019-05-20 10:05:39 +0200

    Add firebase methods for creating user, sign in user and sign out user

--- lib/src/providers/auth_provider.dart
+++ lib/src/providers/auth_provider.dart
@@ -3,23 +3,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'auth_validators.dart';
 export 'auth_validators.dart';
 
+enum Status { Authenticated, Authenticating, Unauthenticated }
 
 class AuthProvider with ChangeNotifier {
 
+  FirebaseAuth _auth;
+  FirebaseUser _user;
+  Status _status = Status.Unauthenticated;
+  
   String _email;
   String _password;
   String _error;
   bool _isLoading = false;
   bool _autovalidate = false;
 
+  AuthProvider.instance() : _auth = FirebaseAuth.instance {
+    _auth.onAuthStateChanged.listen(_onAuthStateChanged);
+  }
+
   String get email => _email;
   String get password => _password;
   String get error => _error;
   bool get isLoading => _isLoading;
   bool get autovalidate => _autovalidate;
-
-  FirebaseAuth get instance => FirebaseAuth.instance;
-  Future<FirebaseUser> get currentUser => instance.currentUser();
+  Status get status => _status;
+  FirebaseUser get user => _user;
 
   set email(String value) {
     _email = value;
@@ -41,9 +49,66 @@ class AuthProvider with ChangeNotifier {
     notifyListeners();
   }
  
-
   set autovalidate(bool value) {
     _autovalidate = value;
+    notifyListeners();
+  }
+
+  Future<bool> signInWithEmailAndPassword({String email, String password}) async {
+    try {
+
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return true;
+      
+    } catch (e) {
+
+      print(e);
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+
+    }
+  }
+
+  Future<bool> createUserWithEmailAndPassword({String email, String password}) async {
+    
+    try {
+
+      _status = Status.Authenticating;
+      notifyListeners();
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return true;
+
+    } catch(e) {
+
+      print(e);
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      return false;
+
+    }
+
+
+  }
+
+  Future<void> signOut() async {
+    _auth.signOut();
+    _status = Status.Unauthenticated;
+    notifyListeners();
+  }
+
+
+  Future<void> _onAuthStateChanged(FirebaseUser firebaseUser) async {
+
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = firebaseUser;
+      _status = Status.Authenticated;
+    }
+
     notifyListeners();
   }
 
