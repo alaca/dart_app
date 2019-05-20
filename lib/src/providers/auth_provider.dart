f205421075beedd264289dcf3b3ae0f8d7e26f4c
alaca <ante.laca@gmail.com>
2019-05-20 12:06:33 +0200

    Add error messages

--- lib/src/providers/auth_provider.dart
+++ lib/src/providers/auth_provider.dart
@@ -13,7 +13,7 @@ class AuthProvider with ChangeNotifier {
   
   String _email;
   String _password;
-  String _error;
+  String _errorCode;
   bool _isLoading = false;
   bool _autovalidate = false;
 
@@ -23,7 +23,7 @@ class AuthProvider with ChangeNotifier {
 
   String get email => _email;
   String get password => _password;
-  String get error => _error;
+  String get error => _getError();
   bool get isLoading => _isLoading;
   bool get autovalidate => _autovalidate;
   Status get status => _status;
@@ -39,11 +39,6 @@ class AuthProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  set error(String value) {
-    _error = value;
-    notifyListeners();
-  }
-
   set isLoading(bool value) {
     _isLoading = value;
     notifyListeners();
@@ -56,7 +51,7 @@ class AuthProvider with ChangeNotifier {
 
   Future<bool> signInWithEmailAndPassword({String email, String password}) async {
     try {
-
+      _errorCode = null;
       _status = Status.Authenticating;
       notifyListeners();
       await _auth.signInWithEmailAndPassword(email: email, password: password);
@@ -64,7 +59,7 @@ class AuthProvider with ChangeNotifier {
       
     } catch (e) {
 
-      print(e);
+      _errorCode = e.code;
       _status = Status.Unauthenticated;
       notifyListeners();
       return false;
@@ -75,19 +70,17 @@ class AuthProvider with ChangeNotifier {
   Future<bool> createUserWithEmailAndPassword({String email, String password}) async {
     
     try {
-
+      _errorCode = null;
       _status = Status.Authenticating;
       notifyListeners();
       await _auth.createUserWithEmailAndPassword(email: email, password: password);
       return true;
 
     } catch(e) {
-
-      print(e);
+      _errorCode = e.code;
       _status = Status.Unauthenticated;
       notifyListeners();
       return false;
-
     }
 
 
@@ -102,15 +95,46 @@ class AuthProvider with ChangeNotifier {
 
   Future<void> _onAuthStateChanged(FirebaseUser firebaseUser) async {
 
-    if (firebaseUser == null) {
-      _status = Status.Unauthenticated;
-    } else {
+    if (firebaseUser != null) {
       _user = firebaseUser;
       _status = Status.Authenticated;
+    } else {
+      _status = Status.Unauthenticated;
     }
 
     notifyListeners();
+    
   }
 
+  String _getError() {
+
+    if (_errorCode == null) 
+      return null;
+
+    switch(_errorCode) {
+
+      case 'ERROR_WEAK_PASSWORD':
+        return 'Password should be at least 6 characters';
+
+      case 'ERROR_INVALID_EMAIL':
+        return 'Invalid email address';
+
+      case 'ERROR_EMAIL_ALREADY_IN_USE':
+        return 'Email already in use';
+
+      case 'ERROR_WRONG_PASSWORD':
+      case 'ERROR_USER_NOT_FOUND':
+      case 'ERROR_INVALID_CREDENTIAL':
+        return 'Wrong email or password';
+
+      case 'ERROR_USER_DISABLED':
+        return 'You are banned!';
+
+      default:
+        return  'Something went wrong';
+
+    }
+
+  }
   
 }
