e7e1dda3cdb163dcb7d37f59726183169ceabc22
alaca <ante.laca@gmail.com>
2019-05-20 12:08:18 +0200

    refactor

--- lib/src/widgets/text.dart
+++ lib/src/widgets/text.dart
@@ -8,15 +8,9 @@ class ErrorText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 15.0),
-      child: Text(
-        this.text, 
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.red
-        )
-      )
+    return Text(
+      this.text, 
+      textAlign: TextAlign.center
     );
   }
 
