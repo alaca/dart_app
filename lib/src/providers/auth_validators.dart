fbc3ed4aa70291d24c3161f566aad3fa614e9971
alaca <ante.laca@gmail.com>
2019-05-20 12:06:57 +0200

    Update email validator regex

--- lib/src/providers/auth_validators.dart
+++ lib/src/providers/auth_validators.dart
@@ -1,12 +1,12 @@
 class AuthValidators {
 
   String validateEmail(String email) {
-    bool isValidEmail = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(email); 
+    bool isValidEmail = RegExp(r"^[a-zA-Z0-9_.]+@[a-zA-Z0-9-_]+\.[a-zA-Z]+").hasMatch(email); 
     return !isValidEmail ? 'Enter valid email' : null;
   }
 
   String validatePassword(String password) {
-    return (password.length <= 3) ? 'Password must be at least 6 characters' : null;
+    return (password.length <= 5) ? 'Password must be at least 6 characters' : null;
   }
 
 }
