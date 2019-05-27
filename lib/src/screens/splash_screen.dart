d25746967d96a83327a675cbeab4b574cd3bde4c
alaca <ante.laca@gmail.com>
2019-05-27 13:56:52 +0200

    Remove Consumer widget

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -9,41 +9,36 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Consumer(
-      builder: (context, AuthProvider auth, _) {
+      AuthProvider auth = Provider.of<AuthProvider>(context);
 
-        Future.delayed(Duration(seconds: 2), () async {
+      Future.delayed(Duration(seconds: 2), () async {
 
-          switch (auth.status) {
-            case Status.Unauthenticated:
-            case Status.Authenticating:
-              return Navigator.pushReplacementNamed(context, 'login');
-            case Status.Authenticated:
-              return Navigator.pushReplacementNamed(context, 'home');
-          } 
+        switch (auth.status) {
+          case Status.Unauthenticated:
+          case Status.Authenticating:
+            return Navigator.pushReplacementNamed(context, 'login');
+          case Status.Authenticated:
+            return Navigator.pushReplacementNamed(context, 'home');
+        } 
 
-        });
+      });
 
-        return Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-              color: Colors.black
-            ),
-            child: Center(
-              child: Hero(
-              tag: 'authTopStaticElements',
-              child: Image.asset( 
-                'assets/images/icon.png',
-                width: 230.0,
-                height: 230.0
-              )
-            ))
-          ) 
-        );
-
-      }
-
-    );
+      return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.black
+          ),
+          child: Center(
+            child: Hero(
+            tag: 'authTopStaticElements',
+            child: Image.asset( 
+              'assets/images/icon.png',
+              width: 230.0,
+              height: 230.0
+            )
+          ))
+        ) 
+      );
 
   }
 
