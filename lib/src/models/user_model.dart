ab9be9fd983633f2c0bd7d785ae52b5f67b3ca82
alaca <ante.laca@gmail.com>
2019-06-10 10:04:19 +0200

    Add popularity prop

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -3,12 +3,14 @@ class UserModel {
   final int id;
   final String image;
   final String name;
+  final double popularity;
 
-  UserModel(this.id, this.image, this.name);
+  UserModel(this.id, this.image, this.name, this.popularity);
 
   UserModel.fromJson(Map<String, dynamic> json )
     : id = json['id'],
-      image = json['profile_path'] ?? '',
-      name = json['name'];
+      image = json['profile_path'] ?? null,
+      name = json['name'],
+      popularity = json['popularity'];
 
 }
